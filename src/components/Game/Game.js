df93c8a7ab8cc401739a61c6cdbf5bfd3563290c
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-01-25 00:59:25 -0600

    updated with exercise 3 completed

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from "../GuessInput";
+import Guess from "../Guess";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
